44c9b20682a2bc31740bc663ca97abad19e56d9c
slower-cuber <slower.cuber@gmail.com>
2023-01-03 22:35:09 +0800

    scan through all possible CO moves of all #QT

--- src/htr.py
+++ src/htr.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
 from twophase import cubie
 
-from src.move import move_cubie
+from src.move import move_cubie, concat_moves
 
 
 HTR_MOVES_SET = [
+    [],
     ["U2"],
     ["R2"],
     ["F2"],
@@ -43,10 +44,42 @@ def bfs_htr(depth: int):
             cp = c.get_corners()
             if cp not in cp2moves:
                 cp2moves[cp] = htr_moves
-        moves_set_of_depth[0] = list(cp2moves.values())
+                moves_set_of_depth[0].append(htr_moves)
+        #moves_set_of_depth[0] = list(cp2moves.values())
+    else:
+        for base_moves in moves_set_of_depth[depth-1]:
+            for u_moves in [["U"], ["U'"]]:
+                for htr_moves in HTR_MOVES_SET:
+                    moves = concat_moves(base_moves, u_moves)
+                    moves = concat_moves(moves, htr_moves)
+                    c = cubie.CubieCube()
+                    move_cubie(c, moves)
+                    cp = c.get_corners()
+                    if cp not in cp2moves:
+                        cp2moves[cp] = moves
+                        moves_set_of_depth[depth].append(moves)
+
 
 
 bfs_htr(0)
 print(moves_set_of_depth)
 print(len(cp2moves))
 
+bfs_htr(1)
+print(moves_set_of_depth)
+print(len(cp2moves))
+
+bfs_htr(2)
+print(len(cp2moves))
+
+bfs_htr(3)
+print(len(cp2moves))
+
+bfs_htr(4)
+print(len(cp2moves))
+
+bfs_htr(5)
+print(len(cp2moves))
+
+bfs_htr(6)
+print(len(cp2moves))
--- src/move.py
+++ src/move.py
@@ -23,9 +23,48 @@ def move_cubie(c: cubie.CubieCube, moves: Union[str, list]):
 
     for m in _moves:
         m = m.strip()
-        c.multiply(
-            cubie.moveCube[STR_2_MOVE[m]]
-        )
+        if m:
+            c.multiply(
+                cubie.moveCube[STR_2_MOVE[m]]
+            )
+
+
+# TODO: cancel more than 1 pair of moves
+def concat_moves(moves_1: list, moves_2: list):
+    def _get_qt(move_word):
+        qt = 0
+        if len(move_word) == 1:
+            qt = 1
+        elif move_word[1] == "2":
+            qt = 2
+        elif move_word[1] == "'":
+            qt = 3
+        return qt
+
+    if not moves_1:
+        return moves_2
+    if not moves_2:
+        return moves_1
+
+    former_tail = moves_1[-1]
+    latter_head = moves_2[0]
+    if former_tail[0] != latter_head[0]:
+        return moves_1 + moves_2
+
+    sum_qt = (_get_qt(former_tail) + _get_qt(latter_head)) % 4
+    if sum_qt == 0:
+        return moves_1[:-1] + moves_2[1:]
+    else:
+        if sum_qt == 1:
+            qt_suffix = ""
+        elif sum_qt == 2:
+            qt_suffix = "2"
+        else:
+            qt_suffix = "'"
+
+        combined_move = former_tail[0] + qt_suffix
+
+        return moves_1[:-1] + [combined_move] + moves_2[1:]
 
 
 def inverse_move_string(move_string: str) -> List:
@@ -37,3 +76,13 @@ def inverse_move_string(move_string: str) -> List:
             yield m[0]
         else:
             yield m
+
+
+if __name__ == '__main__':
+    print(concat_moves(["R", "U"], ["R'", "U'"]))
+    print(concat_moves(["R", "U"], ["U'", "R'"]))
+    print(concat_moves(["R", "U2"], ["U2", "R'"]))
+    print(concat_moves(["R", "U'"], ["U", "R'"]))
+    print(concat_moves(["R", "U'"], ["U'", "R'"]))
+    print(concat_moves(["R", "U2"], ["U'", "R'"]))
+    print(concat_moves(["R", "U2"], ["U", "R'"]))
